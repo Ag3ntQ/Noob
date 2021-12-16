4be1f55ef5d2326b7fe90e5e6faee014e3617ce7
Ag3ntQ <95140254+Ag3ntQ@users.noreply.github.com>
2021-12-16 19:28:31 +0530

    Add files via upload

---
+++ noob.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+figlet Noob.sh
+
+sleep 3
+clear
+echo "           https://github.com/Ag3ntQ"
+sleep 2
+pkg install figlet
+sleep 2
+clear
+figlet 5
+sleep 2
+pkg install python3
+clear
+figlet 4
+sleep 2
+termux-setup-storage
+clear
+figlet 3
+sleep 2
+pkg install php
+clear
+figlet 2
+sleep 2
+pkg install wget
+clear
+figlet 1
+pkg install openssh
+clear
